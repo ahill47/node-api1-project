78b9bdb25901028826df0a29ad81a713919bb20d
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-06-04 20:41:04 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const server = express ();
+
+server.get('/', (req, res)=>{
+
+})
+
+
+
+server.listen(5000, () =>
+console.log('Server running on http://localhost:500')
+);
